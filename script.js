5134045af726eb478d1b362ac2a9d4b89693816f
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-09 22:37:32 +0300

    Task 1-3

---
+++ script.js
@@ -0,0 +1,42 @@
+// Задание 1
+
+document.write('Задание 1. Отнимает от первого числа второе и делит на третье.<br>');
+function calcNumbers(x, y, z) {
+    return (x - y) / z;
+}
+document.write('Получилось число: ', calcNumbers(10, 2, 2), '<br>')
+
+// Задание 2
+
+document.write('<br>Задание 2. Возвращает куб числа и его квадрат.<br>');
+let square, cuba;
+function calcNumbers1(x) {
+    square = x*x;
+    cuba = x*x*x;
+}
+calcNumbers1(3);
+document.write('Квадрат числа: ', square, '. Куб числа: ', cuba, '.<br>')
+
+// Задание 3
+
+document.write('<br>Задание 3. Возвращает меньшее и большее из чисел.<br>');
+let a = 23, b = 11;
+function max(a, b){
+    if (a > b){ 
+        return a
+    } else  if (a < b) {
+        return b
+    } else {
+        document.write('Числа равны.<br>');
+    }
+}
+function min(a, b){
+    if (a < b){ 
+        return a
+    } else  if (a > b) {
+        return b
+    } else {
+        document.write('Числа равны.<br>');
+    }
+}
+document.write('Большее число из функции max: ', max(a, b), '. Меньшее число из функции min: ', min(a, b), '.<br>')
